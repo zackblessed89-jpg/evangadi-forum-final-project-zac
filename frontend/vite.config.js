0565e164d31056ea3a321d13e7fab38be6378867
zackblessed89-jpg <zackblessed89@gmail.com>
2026-03-04 23:58:50 -0800

    configure vite for github pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base:'/evangadi-forum-project-group-1-zac/',
   resolve: {
     alias: {
       // This tells Vite that "@" means the "src" folder
